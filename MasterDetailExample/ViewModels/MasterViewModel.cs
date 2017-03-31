52e7ea14a0617065aedcfc338e8c735722949667
JuneCho23 <junecho.ucb@gmail.com>
2017-03-31 09:30:55 -0700

    Custom Master Details Page for Xamarin.Forms

---
+++ MasterDetailExample/ViewModels/MasterViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+
+using MasterDetailExample.Models;
+using MasterDetailExample.Pages;
+
+namespace MasterDetailExample.ViewModels
+{
+	public class MasterViewModel : BaseViewModel
+	{
+		public MasterViewModel(MasterDetailPage masterPage)
+		{
+			masterDetailPage = masterPage;
+
+			var firstGroup = new GroupedMenuOption() { GroupName = "Group 1" };
+			firstGroup.Add(new MenuOption("Option 1", new NavigationPage(new Page1())));
+			firstGroup.Add(new MenuOption("Option 2", new NavigationPage(new Page2())));
+			firstGroup.Add(new MenuOption("Option 3", new NavigationPage(new Page3())));
+			var secondGroup = new GroupedMenuOption() { GroupName = "Group 2" };
+			secondGroup.Add(new MenuOption("Option 1", new NavigationPage(new Page1())));
+			secondGroup.Add(new MenuOption("Option 2", new NavigationPage(new Page2())));
+			secondGroup.Add(new MenuOption("Option 3", new NavigationPage(new Page3())));
+
+			MenuOptions.Add(firstGroup);
+			MenuOptions.Add(secondGroup);
+
+			masterDetailPage.Detail = MenuOptions[0][0].OptionPage;
+		}
+
+		bool menuPresented;
+		MenuOption selectedMenuOption;
+		MasterDetailPage masterDetailPage;
+
+		public MenuOption SelectedMenuOption
+		{
+			get { return selectedMenuOption; }
+			set
+			{
+				selectedMenuOption = value;
+				OnPropertyChanged();
+
+				if (selectedMenuOption != null)
+					handleNavigation();
+			}
+		}
+
+		public bool MenuPresented
+		{
+			get { return menuPresented; }
+			set
+			{
+				menuPresented = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ObservableCollection<GroupedMenuOption> MenuOptions { get; set; } = new ObservableCollection<GroupedMenuOption>();
+
+		void handleNavigation()
+		{
+			masterDetailPage.Detail = SelectedMenuOption.OptionPage;
+
+			SelectedMenuOption = null;
+			MenuPresented = false;
+		}
+	}
+}
